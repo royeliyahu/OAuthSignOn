8d6cfa5a5c6b8c8d0d31b7b5c7bf19c926240d3f
Roy <royeliyahu827@gmail.com>
2020-06-01 20:41:58 +0300

    Initial commit

---
+++ src/main/java/com/example/OAuthSignOn/OAuthSignOnApplication.java
@@ -0,0 +1,15 @@
+package com.example.OAuthSignOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso//oauth authentication
+public class OAuthSignOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthSignOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OAuthSignOn/OAuthSignOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OAuthSignOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthSignOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
